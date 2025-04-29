12b655bb9ef87dd8244400fc32b620084fd760a8
Konrad <ninianob@gmail.com>
2025-04-29 19:13:37 +0200

    Seeder1

--- HrApp.Infrastructure/Seeder/HrAppSeeder.cs
+++ HrApp.Infrastructure/Seeder/HrAppSeeder.cs
@@ -1,6 +1,8 @@
-﻿using System.Security.Cryptography;
+﻿using System.Data;
+using System.Security.Cryptography;
 using System.Text;
 using Bogus;
+using HrApp.Domain.Constants;
 using HrApp.Domain.Entities;
 using HrApp.Infrastructure.Presistance;
 
@@ -13,28 +15,69 @@ public class HrAppSeeder(HrAppContext dbContext) : IHrAppSeeder
 
     public async Task Seeder()
     {
-        if (_dbContext.Role.Any())
+        if (!_dbContext.Role.Any())
         {
             var roles = GetRoles();
             _dbContext.AddRange(roles);
             await _dbContext.SaveChangesAsync();
         }
 
-        if(_dbContext.User.Any())
+        if(!_dbContext.User.Any())
         {
             var users = GetUsers();
 
-            var roles = _dbContext.Role.ToArray();
+            var role = _dbContext.Role.ToArray();
             var leaves = GetLeaves();
+            var empHist = GetEmploymentsHistory();
+            var workLog = GetWorkLog();
+            var paids = GetPaids();
+            var rates = GetRates();
 
-            for(int i = 0; i < users.Count; i++)
+            for (int i = 0; i < users.Count; i++)
             {
+                if (i == 0)
+                    users[i].Roles.Add(role.FirstOrDefault(x => x.Name == Roles.Ceo.ToString())!);
+                else if(i == 1)
+                    users[i].Roles.Add(role.FirstOrDefault(x => x.Name == Roles.TeamLeader.ToString())!);
+                else if (i == 2)
+                    users[i].Roles.Add(role.FirstOrDefault(x => x.Name == Roles.Hr.ToString())!);
+                else
+                {
+                    var excludedRoles = new[]
+                    {
+                        Roles.Ceo.ToString(),
+                        Roles.TeamLeader.ToString(),
+                        Roles.Hr.ToString()
+                    };
+
+                    var otherRoles = role.Where(x => !excludedRoles.Contains(x.Name)).ToList();
+                    var roleIndex = Random.Shared.Next(otherRoles.Count());
+                    users[i].Roles = [otherRoles[roleIndex]];
+                }
+
                 users[i].Leaves.Add(leaves[i]);
-                
+                users[i].EmploymentHistories.Add(empHist[i]);
+                users[i].WorkLogs = workLog;
+                users[i].Paid = paids[i];
+                rates[i].RatedBy = users[0];
+                users[i].EmployeeRates.Add(rates[i]);
+
+                _dbContext.Leave.AddRange(leaves);
+                _dbContext.EmploymentHistory.AddRange(empHist);
+                _dbContext.WorkLog.AddRange(workLog);
+                _dbContext.Paid.AddRange(paids);
+                _dbContext.EmployeeRate.AddRange(rates);
             }
+            _dbContext.User.AddRange(users);
+            await _dbContext.SaveChangesAsync();
 
         }
-        throw new NotImplementedException();
+
+        if(!_dbContext.Team.Any())
+        {
+
+            Team team = new Team();
+        }
     }
 
     private List<Role> GetRoles()
@@ -91,6 +134,32 @@ public class HrAppSeeder(HrAppContext dbContext) : IHrAppSeeder
     private List<WorkLog> GetWorkLog()
     {
         var workLog = new Faker<WorkLog>(Locale)
-            .RuleFor(x => x.StartTime, y => y.Date.Between(new DateTime(2025,4,14)))
+            .RuleFor(x => x.StartTime, y => y.Date.Between(new DateTime(2025, 4, 1), new DateTime(2025, 4, 20)))
+            .RuleFor(x => x.EndTime, (y, x) => x.StartTime.AddHours(y.Random.Int(1, 8)))
+            .RuleFor(x => x.Hours, (y, x) => (int)(x.EndTime! - x.StartTime).Value.TotalHours)
+            .Generate(5);
+
+        return workLog;
     }
+
+    private List<Paid> GetPaids()
+    {
+        var paids = new Faker<Paid>(Locale)
+            .RuleFor(x => x.BaseSalary, y => y.Random.Float(3000, 10000))
+            .Generate(10);
+
+        return paids;
+    }
+
+    private List<EmployeeRate> GetRates()
+    {
+        var rates = new Faker<EmployeeRate>(Locale)
+            .RuleFor(x => x.Rate, y => y.Random.Int(1, 5))
+            .RuleFor(x => x.RateDate, y => y.Date.RecentDateOnly())
+            .Generate(10);
+
+        return rates;
+    }
+
+
 }
