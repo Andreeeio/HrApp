b9734ef486c9f874099a1f1a02c0c66ab1d494e5
Konrad <ninianob@gmail.com>
2025-06-07 13:57:00 +0200

    v2

--- src/HrApp.Application/Offer/Command/CreateJobApplication/CreateJobApplicationCommandHandler.cs
+++ src/HrApp.Application/Offer/Command/CreateJobApplication/CreateJobApplicationCommandHandler.cs
@@ -55,7 +55,7 @@ namespace HrApp.Application.Offer.Command.CreateJobApplication
                 if (!File.Exists(filePath))
                     throw new BadRequestException("Wystąpił błąd podczas zapisu pliku CV. Spróbuj ponownie.");
                 
-                request.CvLink = "/uploads/cvs/" + uniqueFileName;
+                request.CvLink = "/uploads/offer/cvs/" + uniqueFileName;
             }
 
             var jobApplication = _mapper.Map<Domain.Entities.JobApplication>(request);
